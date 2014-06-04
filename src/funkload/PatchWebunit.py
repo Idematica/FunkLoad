2c2f9214f807260fac094ff02832e329470d6c97
Jeroen Oldenburger <jeroen@socradev.com>
2014-06-04 21:39:00 +0200

    remove double Host header

--- src/funkload/PatchWebunit.py
+++ src/funkload/PatchWebunit.py
@@ -390,10 +390,6 @@ def WF_fetch(self, url, postdata=None, server=None, port=None, protocol=None,
     # Other Full Request headers
     if self.authinfo:
         headers.append(('Authorization', "Basic %s"%self.authinfo))
-    if not webproxy:
-        # HTTPConnection seems to add a host header itself.
-        # So we only need to do this if we are not using a proxy.
-        headers.append(('Host', host_header))
 
     # FL Patch -------------------------
     for key, value in self.extra_headers:
